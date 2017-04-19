672caa3b2f573b2b43a2d3c19ee2b0e6116ba451
gkcsjk <gkcsjk@gmail.com>
2017-04-19 18:02:04 +1000

    database v0.1

--- robot/admin.py
+++ robot/admin.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Motion)
+admin.site.register(MotionList)
+admin.site.register(Walk)
+admin.site.register(Stand)
+admin.site.register(Rest)
+admin.site.register(Speak)
+admin.site.register(Lay)
+admin.site.register(Customize)
--- robot/models.py
+++ robot/models.py
@@ -4,3 +4,41 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class MotionList(models.Model):
+    len = models.IntegerField(default=0)
+
+
+class Motion(models.Model):
+    list_id = models.ForeignKey(MotionList, on_delete=models.CASCADE)
+    order = models.IntegerField(default=0)
+
+
+class Walk(models.Model):
+    motion = models.OneToOneField(Motion, on_delete=models.CASCADE, primary_key=True)
+    vertical = models.FloatField(default=0)
+    horizontal = models.FloatField(default=0)
+    angle = models.FloatField(default=0)
+
+class Customize(models.Model):
+    motion = models.OneToOneField(Motion, on_delete=models.CASCADE, primary_key=True)
+    file_path = models.FileField(upload_to='uploads/')
+    loops = models.IntegerField(default=500)
+
+class Speak(models.Model):
+    motion = models.OneToOneField(Motion, on_delete=models.CASCADE, primary_key=True)
+    text = models.CharField(max_length=1024)
+
+
+class Stand(models.Model):
+    motion = models.OneToOneField(Motion, on_delete=models.CASCADE, primary_key=True)
+
+
+class Rest(models.Model):
+    motion = models.OneToOneField(Motion, on_delete=models.CASCADE, primary_key=True)
+
+
+class Lay(models.Model):
+    motion = models.OneToOneField(Motion, on_delete=models.CASCADE, primary_key=True)
+
+
